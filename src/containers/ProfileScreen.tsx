66f69dd680e45748b17bb527eedce17007aaee0e
Cedric Chau <cedricchau87@gmail.com>
2022-07-17 22:13:21 +0200

    add(Profile): profile screen empty

---
+++ src/containers/ProfileScreen.tsx
@@ -0,0 +1,13 @@
+// ProfileScreen.js
+
+import React from "react";
+import { Text, View, TouchableOpacity } from "react-native";
+
+const ProfileScreen = ({ navigation }: any) => {
+    return (
+        <View>
+            <Text>This is the ProfileScreen component</Text>
+        </View>
+    );
+};
+export default ProfileScreen;
